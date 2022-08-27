ba67078982b7cb500b8f31da990b0729bfeb07de
ZombieGhostMG <zombieghostmg@gmail.com>
2022-08-27 18:10:10 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Types, AptosClient } from 'aptos';
+
+// Create an AptosClient to interact with devnet.
+const client = new AptosClient('https://fullnode.devnet.aptoslabs.com/v1');
+
 function App() {
+
+  // Retrieve aptos.account on initial render and store it.
+  const [address, setAddress] = React.useState<string | null>(null);
+  React.useEffect(() => {
+    window.aptos.account().then((data : {address: string}) => setAddress(data.address));
+  }, []);
+
+  const [account, setAccount] = React.useState<Types.AccountData | null>(null);
+  React.useEffect(() => {
+    if (!address) return;
+    client.getAccount(address).then(setAccount);
+  }, [address]);
+
+  const [modules, setModules] = React.useState<Types.MoveModule[]>([]);
+  React.useEffect(() => {
+    if (!address) return;
+    client.getAccountModules(address).then(setModules);
+  }, [address]);
+
+  const hasModule = modules.some((m) => m.abi?.name === 'Message');
+  const publishInstructions = (
+    <pre>
+      Run this command to publish the module:
+      <br />
+      aptos move publish --package-dir /path/to/hello_blockchain/
+      --named-addresses HelloBlockchain={address}
+    </pre>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!hasModule && publishInstructions}
     </div>
   );
 }
